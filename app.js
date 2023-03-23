0cca4cbc0fadf8044ffd5897dbc9650e9238fc95
shukurullox0507 <shukurullox0507@gmail.com>
2023-03-23 12:06:43 +0500

    generate password function

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const generateDiv = document.querySelector('.generate-div'),
     copyDiv = document.querySelector('.copy-div'),
     range = document.querySelector('.range'),
-    number = document.querySelector('.number');
+    number = document.querySelector('.number'),
+    uppercaseEl = document.querySelector('.uppercase'),
+    lowercaseEl = document.querySelector('.lowercase'),
+    digitEl = document.querySelector('.include-number'),
+    symbolEl = document.querySelector('.symbols'),
+    generateEl = document.querySelector('.generate-btn');
+
+
+
+
 range.addEventListener('input', () => {
     number.textContent = range.value;
 });
+function getRandomChar(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function generatePassword(length, includeUppercase, includeLowercase, includeDigit, includeSymbol) {
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    const digitChars = '0123456789'.split('');
+    const symbolChars = '`~!@#$%^&*()-_=+[{]}\\|;:\'",<.>/?'.split('');
+}
+
