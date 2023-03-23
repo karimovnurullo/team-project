a6b710b5a5206689bae7e9c57bd0e23cafcd1475
bekzod2002 <bekzodistamov179@gmail.com>
2023-03-23 11:07:54 +0500

    renge function

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 const generateDiv = document.querySelector('.generate-div'),
-    copyDiv = document.querySelector('.copy-div');
+    copyDiv = document.querySelector('.copy-div'),
+    range = document.querySelector('.range'),
+    number = document.querySelector('.number');
+range.addEventListener('input', () => {
+    number.textContent = range.value;
+});
