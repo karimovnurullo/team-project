6fa2413de906b37349cfa7dba9c694b3158ba814
shukurullox0507 <shukurullox0507@gmail.com>
2023-03-23 12:33:23 +0500

    generate password if statements

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 const generateDiv = document.querySelector('.generate-div'),
     copyDiv = document.querySelector('.copy-div'),
     range = document.querySelector('.range'),
-    number = document.querySelector('.number'),
-    uppercaseEl = document.querySelector('.uppercase'),
-    lowercaseEl = document.querySelector('.lowercase'),
-    digitEl = document.querySelector('.include-number'),
-    symbolEl = document.querySelector('.symbols'),
-    generateEl = document.querySelector('.generate-btn');
-
-
-
-
-range.addEventListener('input', () => {
+    number = document.querySelector('.number');
+    range.addEventListener('input', () => {
     number.textContent = range.value;
 });
 const uppercaseChekbox = document.querySelector('uppercase-chekbox'),
@@ -28,5 +19,20 @@ function generatePassword(length, includeUppercase, includeLowercase, includeDig
     const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz'.split('');
     const digitChars = '0123456789'.split('');
     const symbolChars = '`~!@#$%^&*()-_=+[{]}\\|;:\'",<.>/?'.split('');
+    const charSets = [];
+
+    if (includeUppercase) {
+        charSets.push(uppercaseChars);
+    }
+    if (includeLowercase) {
+        charSets.push(lowercaseChars);
+    }
+    if (includeDigit) {
+        charSets.push(digitChars);
+    }
+    if (includeSymbol) {
+        charSets.push(symbolChars);
+    }
+
 }
 
