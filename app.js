6f5b18acb982e6b21c92bd31fae9a16a426cffc4
komolxoja <komolxoja2007@gmail.com>
2023-03-23 12:12:01 +0500

    o'zgaruvchilar qo'shildi

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ const generateDiv = document.querySelector('.generate-div'),
 range.addEventListener('input', () => {
     number.textContent = range.value;
 });
+const uppercaseChekbox = document.querySelector('uppercase-chekbox'),
+    lowercaseChekbox = document.querySelector('lowercase-checbox'),
+    numberChekbox = document.querySelector('number-checbox'),
+    symbolChekbox = document.querySelector('symbol-checbox');
